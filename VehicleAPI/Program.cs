696dc315e77c7bc17aa23c2d93c2564da3c0ec8a
naseemhasasneh <naseemhasasnh@gmail.com>
2024-10-05 05:16:57 -0700

    edit docker file , edit program.cs to run swagger on prod.

--- VehicleAPI/Program.cs
+++ VehicleAPI/Program.cs
@@ -14,15 +14,23 @@ builder.Services.AddHttpClient<VehicleService>();
 builder.Services.AddScoped<IVehicleService, VehicleService>();
 // Bind ApiSettings from appsettings.json
 builder.Services.Configure<ApiUrls>(builder.Configuration.GetSection("ApiUrls"));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
+app.UseCors("AllowAll");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
